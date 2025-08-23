d12b78aea58ff567666f86b5880503c3fc3cfd83
ShihabSarar12 <shihabsararchamok@gmail.com>
2025-08-23 21:38:25 +0600

    initialized frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1 class='text-3xl text-amber-800 font-bold underline'>
+                Hello world!
+            </h1>
+        </>
+    );
+}
+
+export default App;
